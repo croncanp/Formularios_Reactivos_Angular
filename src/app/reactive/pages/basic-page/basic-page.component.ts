ea45bcf66d8341df031158f6ec9abff38398e196
Cristian Roncancio <croncanp@contratista.epm.co>
2024-06-18 15:26:03 -0500

    Finalización del codigo de la sección 17 en la cual se incluyeron varias validaciones con el validator errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from 'express';
+
+const initializateForm = {
+  name: '',
+  price: 0,
+  inStorage: 0
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
+
+  // Esta es una de las maneras para trabajar con los formularios reactivos
+  /*public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+  });*/
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset(initializateForm);
+  }
+
+  isValidField(fieldName: string): null | boolean {
+    return this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched;
+  }
+
+  // Manejo de errores del formulario utilizando ValidationErrors
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.contains(fieldName)) return null;
+    const errors = this.myForm.controls[fieldName].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key){
+        case 'required': return 'Este campo es requerido';
+        case 'minlength': return `Este campo es debe terner minimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return '';
+  }
+
+  // Esta es otra manera de trabajar con formularios reactivos y se incluyen las validaciones con el obj validators
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
+  })
+
+  onSave():void{
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset({price: 0, inStorage: 0});
+  }
 
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  public form: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  });
+
+  public newFavorite: FormControl = new FormControl('',[Validators.required]);
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    (this.form.controls['favoriteGames'] as FormArray) = this.formBuilder.array([]);
+    this.form.reset();
+  }
+
+  get favoriteGamesControls() {
+    return this.form.get('favoriteGames') as FormArray;
+  }
+
+  isValidField(fieldName: string): null | boolean {
+    return this.form.controls[fieldName].errors && this.form.controls[fieldName].touched;
+  }
+
+  // Manejo de errores del formulario utilizando ValidationErrors
+  getFieldError(fieldName: string): string | null {
+    if (!this.form.contains(fieldName)) return null;
+    const errors = this.form.controls[fieldName].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key){
+        case 'required': return 'Este campo es requerido';
+        case 'minlength': return `Este campo es debe terner minimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return '';
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number): boolean | null {
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+
+  onDeleteFavorite(index: number):void{
+    this.favoriteGamesControls.removeAt(index);
+  }
+
+  onAddToFavorites():void {
+    if(this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+    this.favoriteGamesControls.push(
+      this.formBuilder.control(newGame, Validators.required)
+    );
+    this.newFavorite.reset();
+  }
 }
